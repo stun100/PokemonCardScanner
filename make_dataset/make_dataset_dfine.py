ba2a68bde024210cc27fedc72b057cb31bbb3c6a
unknown <anthony.322dp@gmail.com>
2024-12-08 20:45:42 +0100

    make_dataset_dfine changes

--- make_dataset/make_dataset_dfine.py
+++ make_dataset/make_dataset_dfine.py
@@ -1,24 +1,24 @@
+import os
 import json
 from tqdm import tqdm
 import cv2
 import numpy as np
 from make_dataset_sample import create_image_with_random_cards
 from make_dataset_util import COORD_K, BBOX_K
-from constants import CARDS_PATH, BG_PATH, OUTPUT_DFINE_PATH
+from constants import CARDS_PATH, BG_PATH, OUTPUT_DFINE_PATH, DFINE_ANNOTATION_OUTPUT
 
 def convert_np_types(obj):
     if isinstance(obj, np.int32):
         return int(obj)
     raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
 
-
-if __name__ == "__main__":
-    # Paths and constants
+def create_dfine_dataset(n_samples, mode):
     bg_folder = BG_PATH
     cards_path = CARDS_PATH
-    output_path = OUTPUT_DFINE_PATH
+    output_path = OUTPUT_DFINE_PATH+"\\"+mode
+    annotation_path = DFINE_ANNOTATION_OUTPUT
 
-    NUM_SAMPLES = 1
+    os.makedirs(output_path, exist_ok=True)
 
     # Initialize COCO-style dataset structure
     coco_dataset = {
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     annotation_id = 0  # Unique ID for each annotation
 
     # Generate samples and populate the dataset
-    for i in tqdm(range(NUM_SAMPLES), desc="Generating Images"):
+    for i in tqdm(range(n_samples), desc=f"Generating Images for {mode}"):
         bg_img_with_cards, card_infos = create_image_with_random_cards(bg_folder, cards_path)
         
         # Save the image
@@ -70,6 +70,18 @@ if __name__ == "__main__":
             coco_dataset["annotations"].append(annotation_entry)
             annotation_id += 1
 
+    annotation_file = f'instances_{mode}.json'
     # Save the dataset to a JSON file
-    with open(f"{output_path}/dataset.json", "w") as outfile:
+    with open(f"{annotation_path}/{annotation_file}", "w") as outfile:
         json.dump(coco_dataset, outfile, indent=4, default=convert_np_types)
+
+
+if __name__ == "__main__":
+    train_examples = 1000
+    create_dfine_dataset(train_examples, 'train')
+
+    val_examples = 200
+    create_dfine_dataset(val_examples, 'val')
+
+    test_examples = 800
+    create_dfine_dataset(test_examples, 'test')
